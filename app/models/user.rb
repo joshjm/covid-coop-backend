17ee791db64e22270cdeb3e3d924adbbff8b622c
Joshua McDonald <mcdojj12@gmail.com>
2020-07-14 10:54:40 +1000

    added comments to complex regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
-validates :username, presence: true
-validates :username, uniqueness: true
-validates :username, length: { minimum: 4 }
-validates :email, presence: true
-validates :email, uniqueness: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 4 }
+  validates :email, presence: true
+  validates :email, uniqueness: true
 
-validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  # prevents people circumventing the input type validation
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-has_many :orders
-has_many :products
+  has_many :orders
+  has_many :products
 end

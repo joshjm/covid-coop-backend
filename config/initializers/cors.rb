df2ccd679b46d2fd4d322cc03bcf3b67dec3aea0
alexmaunder <alex_maunder@hotmail.com>
2020-07-15 15:11:13 +1000

    updated URL to github pages

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env === 'production'
-      origins 'http://covid-co-op.herokuapp.com' # this needs an explicit list on Heroku
+      origins 'https://joshjm.github.io/Covid-Co-op/' # this needs an explicit list on Heroku
     else
       origins 'http://localhost:3001' # this needs an explicit list on Heroku
     end

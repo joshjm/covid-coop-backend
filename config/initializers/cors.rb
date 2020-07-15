84e0518f0ecfa447d32ead471c5e5bd68259e55b
Shahwaiz Qureshi <shay.que.official@gmail.com>
2020-07-15 11:51:18 +1000

    adding frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*' # this needs an explicit list on Heroku
+    origins '*', 'https://joshjm.github.io/' # this needs an explicit list on Heroku
     resource '*',
       :headers => :any,
       :methods => %i( get post put patch delete options head ),
